f50daab5b0e8365d0ad4c25c2885b474dad4caec
DancingChochol <jw303282@student.polsl.pl>
2025-05-13 20:57:25 +0200

    Small fix

--- EkstraSim.Frontend/obj/Release/net9.0/EkstraSim.Frontend.AssemblyInfo.cs
+++ EkstraSim.Frontend/obj/Release/net9.0/EkstraSim.Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EkstraSim.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e7d7dec84b4a24ff816911447f35c0436c9b745")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80153a2b734e624be8dc44a79f8cb13f20a36139")]
 [assembly: System.Reflection.AssemblyProductAttribute("EkstraSim.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EkstraSim.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
